38aa4bb2bbe7a545c5302fce7a062023452531ef
GummaeN <48993780+GummaeN@users.noreply.github.com>
2024-01-24 21:51:59 +0100

    Split notebook into py files

---
+++ src/rasterize.py
@@ -0,0 +1,269 @@
+
+
+def load_params(data, train = True):
+   #data = tf.io.parse_single_example(data, features_description)
+    params_dict = {
+        'scenario_id': data["scenario/id"].numpy(),
+        'agent_id':  data["state/id"].numpy(),
+        'tracks_to_pred': data["state/tracks_to_predict"].numpy(),
+        'width' : data["state/current/width"].numpy(),
+        'length': data["state/current/length"].numpy(),
+        'agent_type': data["state/type"].numpy(),
+        'past_x': data["state/past/x"].numpy(),
+        'past_y': data["state/past/y"].numpy(),
+        'past_yaw': data["state/past/bbox_yaw"].numpy(),
+        'past_valid': data["state/past/valid"].numpy(),
+        'current_x': data["state/current/x"].numpy(),
+        'current_y': data["state/current/y"].numpy(),
+        'current_yaw': data["state/current/bbox_yaw"].numpy(),
+        'current_valid': data["state/current/valid"].numpy(),
+
+        'road_xyz': data["roadgraph_samples/xyz"].numpy(),
+        'road_type': data["roadgraph_samples/type"].numpy(),
+        'road_valid': data["roadgraph_samples/valid"].numpy(),
+        'road_id': data["roadgraph_samples/id"].numpy(),
+
+        'tl_state': data["traffic_light_state/current/state"].numpy(),
+        'tl_id': data["traffic_light_state/current/id"].numpy(),
+        'tl_valid': data["traffic_light_state/current/valid"].numpy()
+        }
+
+    if train:
+
+        tmp_dict = {'future_x': data["state/future/x"].numpy(),
+                    'future_y': data["state/future/y"].numpy(),
+                    'future_valid': data["state/future/valid"].numpy()}
+        params_dict.update(tmp_dict)
+
+
+    return params_dict
+
+
+
+def r_mat(yaw):
+    r_mat = np.array([
+                [np.cos(-yaw), -np.sin(-yaw)],
+                [np.sin(-yaw), np.cos(-yaw)]])
+    return r_mat
+
+from google.colab.patches import cv2_imshow
+
+def rasterize(params, track_to_pred, train = 'True'):
+  #Input params, output rastered params ((25,224,224), gt)
+  #Input params, output rastered params ((25,224,224), gt)
+  #Input params, output rastered params ((13,128,128), gt)
+  # (1:3,224,224) represent roadgraphs and trafic lights
+  # (4:14,224,224) Track to predict agent history xy
+  # (15:25,224,224) All other agents history xy
+
+
+  raster_size = 224
+  ego_center=np.array([0.3, 0.5])
+  pixel_scale=0.5
+  hist_channels = 5
+
+  agents_xy = np.concatenate(
+        (np.expand_dims(np.concatenate((params['past_x'],params['current_x']), axis=1), axis=-1),
+            np.expand_dims(np.concatenate((params['past_y'], params['current_y']), axis=1), axis=-1)
+        ),
+        axis=-1,
+    )
+
+  agents_valid = np.concatenate((params['past_valid'], params['current_valid']), axis=1)
+  agents_id = params['agent_id']
+
+  if train:
+    gt_xy = np.concatenate(
+          (np.expand_dims(params['future_x'], axis=-1), np.expand_dims(params['future_y'], axis=-1)), axis=-1
+      )
+  else:
+    trans_gt_xy = []
+
+
+  agents_yaw = np.concatenate((params['past_yaw'], params['current_yaw']), axis=1)
+
+
+  yaw = agents_yaw[:,10][track_to_pred > 0]
+
+
+
+  road_xy = (params['road_xyz'][:, :2][params['road_valid'].reshape(-1) > 0])
+  road_types = params['road_type'][params['road_valid'] > 0]
+  road_id = params['road_id'][params['road_valid'] > 0]
+
+
+  rot_agents_xy = (agents_xy/pixel_scale) @ np.transpose(r_mat(yaw))
+  shift_xy = rot_agents_xy[:,10][track_to_pred > 0] - raster_size*ego_center
+  trans_agents_xy = rot_agents_xy - shift_xy
+  if train:
+    trans_gt_xy = ((gt_xy/pixel_scale) @ np.transpose(r_mat(yaw))) - shift_xy
+  trans_road_xy = np.squeeze(((road_xy/pixel_scale)  @ np.transpose(r_mat(yaw)) - shift_xy))
+  trans_agents_yaw = agents_yaw-yaw
+
+
+
+
+  road_channels = np.zeros((raster_size, raster_size, 3), dtype=np.uint8)
+
+  track_channels = [
+      np.zeros((raster_size, raster_size,1), dtype=np.uint8)
+      for _ in range(hist_channels)
+  ]
+  agent_channels = [
+      np.zeros((raster_size, raster_size,1), dtype=np.uint8)
+      for _ in range(hist_channels)
+  ]
+
+
+  RoadColorMap = {1:16, 2:16,3:16,
+                11: 28,
+                7 : 40,
+                9:64,
+                6: 88,
+                12: 112,
+                8: 136,
+                10: 160,
+                13: 184,
+                16: 214,
+                15: 255,
+                17: 509,
+                18: 459,
+                19: 479,
+                4:128,
+                5:128,
+                14:128,
+                20:128
+              }
+
+
+  for id in np.unique(road_id):
+    roadline = trans_road_xy[road_id == id]
+    line_type = road_types[road_id == id]
+    color = pd.Series(line_type).map(RoadColorMap)
+    color[np.isnan(color)] = 0
+
+
+    if color[0] < 256:
+      road_channels = cv2.polylines(
+                      road_channels,
+                      [roadline.astype(int)],
+                      False,
+                  (0,0,int(color[0]))
+
+                  )
+    else:
+       road_channels = cv2.polylines(
+                      road_channels,
+                      [roadline.astype(int)],
+                      False,
+                  (int(color[0])-255,0,0)
+                  )
+
+  TlColorMap = {0:0,
+                1:255,
+                2:383,
+                3: 510,
+                4: 255,
+                5: 383,
+                6: 510,
+                7: 255,
+                8: 383,
+              }
+
+  for id in np.unique(params['tl_id']):
+      roadline = trans_road_xy[road_id == id]
+      line_type = params['tl_state'][params['tl_id'] == id]
+      color = pd.Series(line_type).map(TlColorMap)
+      color[np.isnan(color)] = 0
+
+
+      if color[0] < 256:
+        road_channels = cv2.polylines(
+                        road_channels,
+                        [roadline.astype(int)],
+                        False,
+                    (int(color[0]),0,0)
+
+                    )
+      else:
+        road_channels = cv2.polylines(
+                        road_channels,
+                        [roadline.astype(int)],
+                        False,
+                    (0,int(color[0])-255,0)
+                    )
+
+
+
+
+  ego_type = params['agent_type'][track_to_pred > 0]
+  ego_id = agents_id[track_to_pred>0]
+  ego_xy = trans_agents_xy[:,:][track_to_pred > 0]
+  ego_w = (np.squeeze(params['width'][track_to_pred > 0]))/pixel_scale
+  ego_l = (np.squeeze(params['length'][track_to_pred > 0]))/pixel_scale
+  bbpoints = np.array([[-ego_l/2,  ego_w/2], [ego_l/2 , ego_w/2], [ego_l/2, -ego_w/2], [-ego_l/2, -ego_w/2]], np.float32)
+  for i in range(hist_channels):
+    for j in range(3):
+      angle = trans_agents_yaw[:,j+1][track_to_pred > 0]
+
+      t_bbpoints = (bbpoints @ np.transpose(r_mat(-angle))) + ego_xy[0,j+1]
+      cv2.fillPoly(
+                        track_channels[i],
+                        t_bbpoints.astype(int),
+                        color= 255-(65*(2-j)),
+                         #color= 60+(45*(i))-(15*(2-j))
+                         )
+
+
+
+  agents_w = (np.squeeze(params['width']))/pixel_scale
+  agents_l = (np.squeeze(params['length']))/pixel_scale
+  unique_agents_id = np.unique(agents_id[agents_valid[:,10] > 0])
+
+  for id in (unique_agents_id):
+    if id != ego_id:
+      bbpoints = np.squeeze(np.array([[-agents_l[agents_id == id]/2, agents_w[agents_id == id]/2],
+                          [agents_l[agents_id == id]/2 , agents_w[agents_id == id]/2],
+                          [agents_l[agents_id == id]/2, -agents_w[agents_id == id]/2],
+                          [-agents_l[agents_id == id]/2, -agents_w[agents_id == id]/2]], np.float32))
+
+      for i in range(hist_channels):
+        for j in range(3):
+          angle = trans_agents_yaw[:,j+1][agents_id == id][0]
+
+          t_bbpoints = (bbpoints @ np.transpose(r_mat(-angle))) + trans_agents_xy[:,j+1][agents_id == id]
+
+          if ((t_bbpoints.max() < raster_size) & (t_bbpoints.min() > 0)):
+            cv2.polylines(
+                              agent_channels[i],
+                              [np.int32([t_bbpoints])],
+                              True,
+                              color= 255-(85*(2-j)))
+
+
+
+
+    raster = np.concatenate([road_channels] + track_channels + agent_channels, axis=2)
+
+    raster_dict = {
+            "track_to_pred": track_to_pred,
+            "raster": raster,
+            "yaw": yaw,
+            "shift": shift_xy,
+            "gt": trans_gt_xy[track_to_pred > 0],
+            "scenario_id": params['scenario_id'],
+            "ego_type": ego_type,
+            "valid": np.squeeze(params['future_valid'][track_to_pred > 0])
+    }
+
+
+  return raster_dict
+
+
+
+def save_raster(raster_dict, step):
+
+  save_dir = '/content/traintar/'
+
+  filename = f"{raster_dict['ego_type']}_{raster_dict['scenario_id']}_{step}.npz"
+  np.savez_compressed(os.path.join(save_dir, filename), **raster_dict)
---
+++ src/visualize.py
@@ -0,0 +1,126 @@
+def visualize(raster, pred, cluster_pred, target):
+
+
+  raster = np.transpose(np.squeeze(raster),(1,2,0))
+  target = np.array(np.squeeze(target))
+  cluster_pred = np.array(np.squeeze(cluster_pred))
+  img = np.zeros((224, 224, 3), dtype=np.uint16)
+  img_over = np.zeros((224, 224, 3), dtype=np.uint16)
+
+
+  img[:,:,0] = raster[:,:,2]*1500
+  img[:,:,1] = raster[:,:,2]*1500
+  img[:,:,2] = raster[:,:,2]*1500
+
+  img[:,:,0][img[:,:,0] > 255] = 170
+  img[:,:,1][img[:,:,1] > 255] = 170
+  img[:,:,2][img[:,:,2] > 255] = 170
+
+
+  img_over[:,:,1] = raster[:,:,7]*255+raster[:,:,12]*200
+  img_over[:,:,2] = (raster[:,:,1]+raster[:,:,0])*255
+  img_over[:,:,2][img_over[:,:,2] > 255] = 255
+  img_over[:,:,1][img_over[:,:,1] > 255] = 255
+
+  img = cv2.addWeighted(img_over, 1, img, 1, 0)
+
+  
+  plt.ioff()
+  px = 1/plt.rcParams['figure.dpi']  # pixel in inches
+  fig, ax = plt.subplots(figsize=(450*px, 450*px))
+  plt.style.use('dark_background')
+  
+  def colorlist2(c1, c2, num):
+      l = np.linspace(0,1,num)
+      a = np.abs(np.array(c1)-np.array(c2))
+      m = np.min([c1,c2], axis=0)
+      s  = np.sign(np.array(c2)-np.array(c1)).astype(int)
+      s[s==0] =1
+      r = np.sqrt(np.c_[(l*a[0]+m[0])[::s[0]],(l*a[1]+m[1])[::s[1]],(l*a[2]+m[2])[::s[2]]])
+      return r
+
+  for i in range(len(cluster_pred)):
+    x = cluster_pred[i,:,0]
+    y = cluster_pred[i,:,1]
+
+    t1 = 	(206,162,253)
+    t2 = 	(83,0,100)
+
+
+    c1 = tuple(i /255 for i in t1)
+    c2 = tuple(i /255 for i in t2)
+
+    cmap = LinearSegmentedColormap.from_list("", colorlist2(c1, 	c2,100))
+
+    points = np.array([x, y]).T.reshape(-1,1,2)
+    segments = np.concatenate([points[:-2],points[1:-1], points[2:]], axis=1)
+
+    lc = LineCollection(segments, cmap=cmap, linewidth=4, alpha = 0.65)
+    lc.set_array(x)
+    line = ax.add_collection(lc)
+  
+
+  x = target[:,0]
+  y = target[:,1]
+  plt.plot(x,y,color = 'yellow',linewidth=2, zorder = 2)
+
+
+  coordList = (29,49,79)
+  th_lat = [2,3.6,6.0]
+  th_long = [4.0,7.2,12]
+  k = 0
+  r = []
+  for i in coordList:
+    
+    x = target[i,0]
+    y = target[i,1]
+
+    hyp = (np.sqrt(((th_lat[k]/2)**2)+((th_long[k]/2)**2)))
+    ang = np.arctan((x-target[i-1,0])/(y-target[i-1,1]))
+  
+    ang2 = -(np.pi-np.arcsin((th_lat[k]/2)/hyp))
+    ang3 = ang2 + ang
+    x0 = x + hyp*np.sin(ang3)
+    y0 = y + hyp*np.cos(ang3)
+
+    r.append(patches.Rectangle((x0,y0),th_lat[k] ,th_long[k],color="yellow", alpha=0.50, fill = True, zorder = 4))
+    t1 = mpl.transforms.Affine2D().rotate_around(x0, y0,-ang)
+    r[k].set_transform(t1 + ax.transData)
+
+    ax.add_patch(r[k])
+
+    k += 1
+
+  plt.xlim([0, 224])
+  plt.ylim([0, 224]) 
+  plt.setp([ax.get_xticklines() + ax.get_yticklines() + ax.get_xgridlines() + ax.get_ygridlines()],antialiased=False)
+  plt.rcParams['text.antialiased']=False
+  ax.axis('off')
+  fig.tight_layout(pad=0)
+
+  ax.margins(0)
+  fig.canvas.draw()
+
+
+
+  data = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+  data = data.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+  data = data.astype(np.uint16)
+  tmp_data = data.copy()
+  data[:,:,0] = tmp_data[:,:,2]
+  data[:,:,2] = tmp_data[:,:,0]
+
+  res = cv2.resize(img, (450,450), interpolation=cv2.INTER_LINEAR)
+
+  data[:,:,0][data[:,:,0] > 255] = 230
+  data[:,:,1][data[:,:,1] > 255] = 255
+  data[:,:,2][data[:,:,2] > 255] = 255
+
+
+  img_new = cv2.addWeighted(data, 1, res, 1, 0)
+
+  img_new[img_new == 0] = 15
+
+
+
+  cv2_imshow(img_new)
